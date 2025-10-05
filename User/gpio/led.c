845010072ea75b160de6b5842562f7f5f92bda7d
inmove <lisper.inmove@gmail.com>
2025-10-05 21:32:12 +0800

    开漏输出 点亮LED

--- User/gpio/led.c
+++ User/gpio/led.c
@@ -10,12 +10,12 @@ void LED_Init(void) {
     // 0引脚配置
     GPIO_InitType.Pin = GPIO_PIN_6;
     // 推挽输出
-    GPIO_InitType.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitType.Mode = GPIO_MODE_OUTPUT_OD;
     // GPIO_SPEED_FREQ_MEDIUM：中等速度。
     // GPIO_SPEED_FREQ_HIGH：高速。
     // GPIO_SPEED_FREQ_VERY_HIGH：非常高速。
     // 低速，降低电源消耗，响应时间较长
     GPIO_InitType.Speed = GPIO_SPEED_FREQ_LOW;
 
-    HAL_GPIO_Init(GPIOA, &GPIO_InitType);
+    HAL_GPIO_Init(GPIOB, &GPIO_InitType);
 }
